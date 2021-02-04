449528fa64b881a7538f25cbdeb464e6c70e5a3f
Benoît Nicol <70569545+Bein54@users.noreply.github.com>
2021-02-04 11:57:39 +0100

    Add files via upload

---
+++ td2-squelette/modules/products.js
@@ -0,0 +1,15 @@
+class Produit {
+    constructor (ref, price, description) {
+        this.ref = ref;
+        this.price = price;
+        this.descritpion = description;
+    }
+}
+
+export default {
+    products : [
+        new Produit("a", 1, "produit1"),
+        new Produit("b", 2, "produit2"),
+        new Produit("c", 3, "produit3")
+    ]
+};
