e3bc2b001d657df4df15bc3880a63f9d000153ae
andrea-crivelli <70567668+andrea-crivelli@users.noreply.github.com>
2021-02-11 09:48:55 +0100

    resolution pb1

--- td2-squelette/modules/products.js
+++ td2-squelette/modules/products.js
@@ -5,16 +5,17 @@ class Produit {
         this.description = description;
     } 
 }
+let products = [
+    new Produit("a", 1, "produit1"),
+    new Produit("b", 2, "produit2"),
+    new Produit("c", 3, "produit3")
+];
 
 function search(keywords) {
     products.filter(e => e.ref>=keywords || e.ref>=keywords);
 }
 
 export default {
-    products : [
-        new Produit("a", 1, "produit1"),
-        new Produit("b", 2, "produit2"),
-        new Produit("c", 3, "produit3")
-    ],
+    products,
     search : search
 };
